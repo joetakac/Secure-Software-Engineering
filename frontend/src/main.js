bec3ea651adeed42a985b8d45b2055f68cd84df2
joet2077 <b8027147@my.shu.ac.uk>
2023-03-21 15:18:56 +0000

    added keycloak login authentication

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import router from './router';
 import './assets/all.css';
+import axios from 'axios';
+import Keycloak from "keycloak-js";
 
+//keycloak realm details
+const keycloakobj = new Keycloak({
+    url: 'http://localhost:8080/',
+    realm: 'sse',
+    clientId: 'sse-client',
+    onLoad: 'login-required'
+  });
 
+keycloakobj.init({ onLoad: 'login-required' }).then(authenticated => {
+    if (authenticated) {
+      // Set the user's access token as a default authorization header
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + keycloakobj.token;
 
-createApp(App).use(router).mount('#app');
+      const app = createApp(App);
+        app.use(router);
+        app.mount('#app');
+    }
+  })
+  
